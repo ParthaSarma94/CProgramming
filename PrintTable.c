57549580c2265eacd5266e6013825c931bd87b66
Partha Sarma <131948502+ParthaSarma94@users.noreply.github.com>
2023-08-26 10:38:15 +0530

    Update PrintTable.c

--- PrintTable.c
+++ PrintTable.c
@@ -5,7 +5,7 @@
 int main(int argc, char const *argv[])
 {
     int n;
-    printf("Enter the number that need to be created a table:\n");
+    printf("Enter the number:\n");
     scanf("%d",&n);
     for (int  i = 1; i <= 10; i++)
     {
